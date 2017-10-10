ff1dce7d708a835e1f0f8d7b8ffaee541c52126a
Jukka Turunen <jukka.turunen@vikaservices.fi>
2017-10-10 17:19:08 +0200

    support box stats font size

--- app/assets/scripts/modules/menu.js
+++ app/assets/scripts/modules/menu.js
@@ -6,8 +6,8 @@ class Menu {
     this.menuContent = document.getElementsByClassName("menu__links")[0];
     // mobile hamburger icon
     this.menuIcon = document.getElementsByClassName("menu__menu-icon")[0];
-    // global menu close handler
-    window.addEventListener("click", this.hideMenu);
+    // content wrapper
+    this.contentWrapper = document.getElementsByClassName("wrapper")[0];
     this.events();
   }
 
@@ -16,6 +16,8 @@ class Menu {
     this.menu.addEventListener("click", this.handleTitleClick.bind(this));
     // click handler for mobile
     this.menuIcon.addEventListener("click", this.handleTitleClick.bind(this));
+    // global menu close handler
+    window.addEventListener("click", this.hideMenu.bind(this));
   }
 
   handleTitleClick(e) {
@@ -24,11 +26,17 @@ class Menu {
     this.menuContent.classList.toggle("menu__links--is-visible");
     // change menu icon
     this.menuIcon.classList.toggle("menu__menu-icon--close-x");
+    // disable scroll
+    document.body.classList.toggle("disable-scroll");
+    // add opacity to rest of document
+    this.contentWrapper.classList.toggle("opaque");
   }
 
   hideMenu() {
     document.getElementsByClassName("menu__links")[0].classList.remove("menu__links--is-visible");
     document.getElementsByClassName("menu__menu-icon")[0].classList.remove("menu__menu-icon--close-x");
+    document.body.classList.remove("disable-scroll");
+    this.contentWrapper.classList.remove("opaque");
   }
 
 }
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -116,8 +116,8 @@ var Menu = function () {
     this.menuContent = document.getElementsByClassName("menu__links")[0];
     // mobile hamburger icon
     this.menuIcon = document.getElementsByClassName("menu__menu-icon")[0];
-    // global menu close handler
-    window.addEventListener("click", this.hideMenu);
+    // content wrapper
+    this.contentWrapper = document.getElementsByClassName("wrapper")[0];
     this.events();
   }
 
@@ -128,6 +128,8 @@ var Menu = function () {
       this.menu.addEventListener("click", this.handleTitleClick.bind(this));
       // click handler for mobile
       this.menuIcon.addEventListener("click", this.handleTitleClick.bind(this));
+      // global menu close handler
+      window.addEventListener("click", this.hideMenu.bind(this));
     }
   }, {
     key: "handleTitleClick",
@@ -137,12 +139,18 @@ var Menu = function () {
       this.menuContent.classList.toggle("menu__links--is-visible");
       // change menu icon
       this.menuIcon.classList.toggle("menu__menu-icon--close-x");
+      // disable scroll
+      document.body.classList.toggle("disable-scroll");
+      // add opacity to rest of document
+      this.contentWrapper.classList.toggle("opaque");
     }
   }, {
     key: "hideMenu",
     value: function hideMenu() {
       document.getElementsByClassName("menu__links")[0].classList.remove("menu__links--is-visible");
       document.getElementsByClassName("menu__menu-icon")[0].classList.remove("menu__menu-icon--close-x");
+      document.body.classList.remove("disable-scroll");
+      this.contentWrapper.classList.remove("opaque");
     }
   }]);
 
